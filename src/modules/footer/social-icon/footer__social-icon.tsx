4fe45f22857d9d1d42fc3dce0fe4a7305970d9f5
Eгор Борисов <nd.5nd@yandex.ru>
2026-04-11 17:55:11 +0300

    adaptation for small PC

--- src/modules/footer/social-icon/footer__social-icon.tsx
+++ src/modules/footer/social-icon/footer__social-icon.tsx
@@ -2,10 +2,10 @@ import FooterSocialIconItem from "./item/footer__social-icon__item";
 
 export default function FooterSocialIcon() {
   return (
-    <>
+    <div className="footer__social-icon">
       <FooterSocialIconItem src="./assets/ig.png" alt="instagram" href="#" />
       <FooterSocialIconItem src="./assets/google.png" alt="google" href="#" />
       <FooterSocialIconItem src="./assets/ball.png" alt="ball" href="#" />
-    </>
+    </div>
   );
 }
